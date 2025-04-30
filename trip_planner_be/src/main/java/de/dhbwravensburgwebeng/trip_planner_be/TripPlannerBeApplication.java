a77fc3e004fd4baca8dbde2b3644ca3174c685d4
Bennet <pascal.meerwasser@gmail.com>
2025-04-30 10:45:36 +0200

    Initial commit

---
+++ trip_planner_be/src/main/java/de/dhbwravensburgwebeng/trip_planner_be/TripPlannerBeApplication.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburgwebeng.trip_planner_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TripPlannerBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TripPlannerBeApplication.class, args);
+	}
+
+}
---
+++ trip_planner_be/src/test/java/de/dhbwravensburgwebeng/trip_planner_be/TripPlannerBeApplicationTests.java
@@ -0,0 +1,13 @@
+package de.dhbwravensburgwebeng.trip_planner_be;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TripPlannerBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
