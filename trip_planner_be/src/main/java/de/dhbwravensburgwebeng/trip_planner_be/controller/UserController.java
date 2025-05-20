6b658f8e93c7f9ceab2174ad0eb274726f6ead17
Bennet <pascal.meerwasser@gmail.com>
2025-05-20 17:12:06 +0200

    signup and login work (still need implementation of jwt)

--- trip_planner_be/src/main/java/de/dhbwravensburgwebeng/trip_planner_be/controller/UserController.java
+++ trip_planner_be/src/main/java/de/dhbwravensburgwebeng/trip_planner_be/controller/UserController.java
@@ -4,23 +4,52 @@ import de.dhbwravensburgwebeng.trip_planner_be.model.HelpFunctions;
 import de.dhbwravensburgwebeng.trip_planner_be.model.UserEntity;
 import de.dhbwravensburgwebeng.trip_planner_be.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
 import java.util.List;
 
 @RestController
 public class UserController {
 
-    @Autowired
-    UserRepository userRepository;
+    private final UserRepository userRepository;
+    private final HelpFunctions helper;
+
+    public UserController(UserRepository userRepository, HelpFunctions helper) {
+        this.userRepository = userRepository;
+        this.helper = helper;
+    };
+
 
     @PostMapping("/signup")
     public UserEntity createUser(@RequestBody @Validated UserEntity user) {
         user.setPassword(HelpFunctions.hash(user.getPassword()));
         userRepository.save(user);
         return user;
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<UserEntity> login(@RequestBody @Validated UserEntity user) {
+        if (!userRepository.existsByEmail(user.getEmail())) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+        if  (helper.checkPassword(user.getPassword(), user.getEmail())){
+            Long userId = userRepository.findIdByEmail(user.getEmail());
+            user.setPassword(HelpFunctions.hash(user.getPassword()));
+            user.setId(userId);
+            user.setTimestamp(new Date().getTime());
+            userRepository.save(user);
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+
+        else {
+            return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+        }
+
     }
 
     @DeleteMapping("/user/id")
--- trip_planner_be/src/main/java/de/dhbwravensburgwebeng/trip_planner_be/model/HelpFunctions.java
+++ trip_planner_be/src/main/java/de/dhbwravensburgwebeng/trip_planner_be/model/HelpFunctions.java
@@ -1,8 +1,19 @@
 package de.dhbwravensburgwebeng.trip_planner_be.model;
 
+import de.dhbwravensburgwebeng.trip_planner_be.repositories.UserRepository;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Component;
 
+import java.util.Optional;
+
+@Component
 public class HelpFunctions {
+
+    private final UserRepository userRepository;
+
+    public HelpFunctions(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     private static final BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
 
@@ -13,7 +24,23 @@ public class HelpFunctions {
     public static boolean matches(String hashed, String value){
         return encoder.matches(hashed, value);
     }
+
+    public boolean checkPassword(String plainPassword, String email) {
+        Optional<UserEntity> userOpt = userRepository.findByEmail(email);
+
+        if (userOpt.isPresent()) {
+            String hashedPassword = userOpt.get().getPassword();
+            return matches(plainPassword, hashedPassword);
+        }
+
+        return false; // user not found
+    }
 }
+//    public boolean checkPassword(String plainPassword, String email){
+//            Optional<UserEntity> user = userRepository.findByEmail(email);
+//            return user.filter(userEntity -> matches(plainPassword, userEntity.getPassword())).isPresent();
+//        }
+//    }
 
 
 //import at.favre.lib.crypto.bcrypt.BCrypt;
