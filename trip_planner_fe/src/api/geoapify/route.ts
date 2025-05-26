4670d28d5e54de8ab587db3cb9c800e863dcd248
Bennet <pascal.meerwasser@gmail.com>
2025-05-26 14:23:50 +0200

    draws route on select of start/end

---
+++ trip_planner_fe/src/api/geoapify/route.ts
@@ -0,0 +1,77 @@
+// for routing
+import maplibregl from "maplibre-gl";
+
+const ROUTING_API_KEY = "7263a7cafcc4410db5377fda5a87d544"
+
+let markers: maplibregl.Marker[] = []
+
+function displayMarker(map: maplibregl.Map, waypoint: [number, number], text: string) {
+    if (map) {
+        const marker = new maplibregl.Marker().setLngLat([waypoint[1], waypoint[0]])
+            .setPopup(new maplibregl.Popup().setText(
+                text
+            )).addTo(map);
+        markers.push(marker);
+    }
+}
+
+function clearMarkers() {
+    markers.forEach(marker => marker.remove());
+    markers = [];
+}
+
+function displayRoute(map: maplibregl.Map, data: { features: any[]; }, start, end) {
+    if (map) {
+        if (map.getLayer("route-layer")) {
+            map.removeLayer("route-layer")
+            map.removeSource("route-layer")
+        }
+        clearMarkers();
+
+        map.addSource('route-layer', {
+            type: 'geojson',
+            data: data.features[0]
+        });
+
+        map.addLayer({
+            'id': 'route-layer',
+            'type': 'line',
+            'source': 'route-layer',
+            'layout': {
+                'line-cap': "round",
+                'line-join': "round"
+            },
+            'paint': {
+                'line-color': "#6084eb",
+                'line-width': 8
+            },
+            'filter': ['==', '$type', 'LineString']
+        });
+
+        displayMarker(map, start, "Berlin")
+        displayMarker(map, end, "Munich")
+    }
+}
+
+export const handleGetRoute = async (map: maplibregl.Map, startInput, endInput)=> {
+    const start: [number, number] = [
+        startInput.properties.lat,
+        startInput.properties.lon
+    ];
+
+    const end: [number, number] = [
+        endInput.properties.lat,
+        endInput.properties.lon
+    ];
+    console.log(start + "start")
+    console.log(end + "end")
+    const response = await fetch(`https://api.geoapify.com/v1/routing?waypoints=${start[0]},${start[1]}|${end[0]},${end[1]}&mode=drive&apiKey=${ROUTING_API_KEY}`, {
+        method: "GET",
+    })
+    if (response.ok) {
+        const data = await response.json()
+
+        displayRoute(map, data, start, end);
+
+    }
+}
--- trip_planner_fe/src/components/mapInput.tsx
+++ trip_planner_fe/src/components/mapInput.tsx
@@ -4,10 +4,27 @@ import {
 } from "@geoapify/react-geocoder-autocomplete";
 import "@geoapify/geocoder-autocomplete/styles/minimal.css";
 import "../css/addressInput.css";
+import {useState} from "react";
+import {handleGetRoute} from "../api/geoapify/route.ts";
+import {useMap} from "../api/geoapify/mapContext.tsx";
 
 const App = () => {
-  function onPlaceSelect(value: any) {
-    console.log(value);
+  const {map} = useMap()
+  const [startInput, setStartInput] = useState();
+  const [endInput, setEndInput] = useState();
+
+  function onPlaceSelectStart(value: any) {
+    setStartInput(value)
+    if(endInput && map){
+      handleGetRoute(map, value, endInput); //value since setStartInput is async and doesnt have value yet
+    }
+  }
+
+  function onPlaceSelectEnd(value :any){
+    setEndInput(value)
+    if(startInput && map){
+      handleGetRoute(map, startInput, value)
+    }
   }
 
   function onSuggectionChange(value: any) {
@@ -19,7 +36,7 @@ const App = () => {
       <div className="address-input-wrapper">
         <h2>Start</h2>
         <GeoapifyGeocoderAutocomplete
-          placeSelect={onPlaceSelect}
+          placeSelect={onPlaceSelectStart}
           suggestionsChange={onSuggectionChange}
         />
       </div>
@@ -27,7 +44,7 @@ const App = () => {
       <div className="address-input-wrapper">
         <h2>Ziel</h2>
         <GeoapifyGeocoderAutocomplete
-          placeSelect={onPlaceSelect}
+          placeSelect={onPlaceSelectEnd}
           suggestionsChange={onSuggectionChange}
         />
       </div>
