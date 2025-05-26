e7bcc4e878c2b36f86edcac730597a156a74d001
Bennet <pascal.meerwasser@gmail.com>
2025-05-26 09:51:23 +0200

    added markers for start and end

--- trip_planner_fe/src/api/geoapify/routing.tsx
+++ trip_planner_fe/src/api/geoapify/routing.tsx
@@ -1,20 +1,33 @@
 // for routing
 import {useMap} from "./mapContext.tsx";
+import maplibregl from "maplibre-gl";
+import {useRef} from "react";
 
 const ROUTING_API_KEY = "7263a7cafcc4410db5377fda5a87d544"
 
-const start = {
-    lat: 52.5200,
-    lon: 13.4050
-}
+const start :[number, number] = [52.5200, 13.4050]
 
-const end = {
-    lat: 48.1351,
-    lon: 11.5820,
-}
+const end:[number, number] = [48.1351, 11.5820]
 
 const Route = () => {
     const {map} = useMap();
+
+    const markersRef = useRef<maplibregl.Marker[]>([]);
+
+    function displayMarker(waypoint :[number, number], text :string) {
+        if(map){
+            const marker = new maplibregl.Marker().setLngLat([waypoint[1], waypoint[0]])
+                .setPopup(new maplibregl.Popup().setText(
+                    text
+                )).addTo(map);
+            markersRef.current.push(marker);
+        }
+    }
+
+    function clearMarkers(){
+        markersRef.current.forEach(marker => marker.remove());
+        markersRef.current = [];
+    }
 
     function displayRoute(data: { features: any[]; }){
         if(map){
@@ -22,6 +35,7 @@ const Route = () => {
                 map.removeLayer("route-layer")
                 map.removeSource("route-layer")
             }
+            clearMarkers();
 
             map.addSource('route-layer', {
                 type: 'geojson',
@@ -42,11 +56,14 @@ const Route = () => {
                 },
                 'filter': ['==', '$type', 'LineString']
             });
+
+            displayMarker(start, "Berlin")
+            displayMarker(end, "Munich")
         }
     }
 
     async function handleGetRoute() {
-        const response = await fetch(`https://api.geoapify.com/v1/routing?waypoints=${start.lat},${start.lon}|${end.lat},${end.lon}&mode=drive&apiKey=${ROUTING_API_KEY}`, {
+        const response = await fetch(`https://api.geoapify.com/v1/routing?waypoints=${start[0]},${start[1]}|${end[0]},${end[1]}&mode=drive&apiKey=${ROUTING_API_KEY}`, {
             method: "GET",
         })
         if (response.ok){
