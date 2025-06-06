080e629d40765c9f76f57bf413f4317050898f03
errormarc <marcritscher@gmail.com>
2025-06-06 21:30:34 +0200

    about and services page with style

--- trip_planner_fe/src/pages/About.tsx
+++ trip_planner_fe/src/pages/About.tsx
@@ -1,14 +1,21 @@
-function About() {
+import React from 'react';
+import '../css/about.css';
+
+const About: React.FC = () => {
   return (
-    <div>
-      <main>
+    <div className="about-container">
+      <main className="about-main">
         <h1>Über uns</h1>
+        <h2>Wer wir sind</h2>
         <p>
-          About
+          Hi! Wir sind ein Team von jungen, reiselustigen Studierenden vom Bodensee,
+          die es sich zur Mission gemacht haben, Reisen einfacher, spannender und individueller zu gestalten.
+          Unsere Idee? Ein Tripplaner, der nicht nur Routen erstellt, sondern euch Tankstellen, Restaurants und mehr
+          auf eurer Route vorschlägt – maßgeschneidert für dich, deine Crew und deinen Vibe.
         </p>
       </main>
     </div>
   );
-}
+};
 
 export default About;
